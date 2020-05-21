bc2220c2fb0a6fcf5cb14af0ed57224db60e56ed
Doowankim <rlaendhks11@gmail.com>
2020-05-21 22:00:12 +0900

    fix: Admin style

--- src/components/Admin/Login.js
+++ src/components/Admin/Login.js
@@ -23,32 +23,70 @@ const Login = () => {
     console.log(loginUser);
   };
   return (
-    <div style={{fontSize: '20px'}}>
-      <div>Login</div>
+    <div style={{padding: '20px'}}>
+      <div
+        style={{
+          fontSize: '22px',
+          color: 'rgb(77, 82, 86)',
+          marginBottom: '15px',
+        }}
+      >
+        Login
+      </div>
       <form onSubmit={onSubmit}>
         <div>
+          <div>
+            <label style={{fontSize: '18px', color: 'rgb(89, 89, 89)'}}>
+              Email
+            </label>
+          </div>
           <input
             type="email"
             value={email}
             placeholder="example@gmail.com"
-            style={{width: '300px', height: '20px'}}
+            style={{width: '300px', height: '25px'}}
             onChange={onChangeEmail}
           />
         </div>
         <div>
+          <div style={{marginTop: '20px'}}>
+            <label
+              style={{
+                fontSize: '18px',
+                color: 'rgb(89, 89, 89)',
+              }}
+            >
+              Password
+            </label>
+          </div>
           <input
             type="password"
             value={password}
             placeholder="*******"
-            style={{width: '300px', height: '20px', marginTop: '10px'}}
+            style={{width: '300px', height: '25px'}}
             onChange={onChangePassword}
           />
         </div>
-        <button type="submit">Confirm</button>
+        <button
+          type="submit"
+          style={{
+            height: '2rem',
+            marginTop: '20px',
+            paddingLeft: '1.25rem',
+            paddingRight: '1.25rem',
+            borderRadius: '4px',
+            cursor: 'pointer',
+          }}
+        >
+          Confirm
+        </button>
+        <Link
+          to="/register"
+          style={{textDecoration: 'none', color: '#9F765E', marginLeft: '5px'}}
+        >
+          If you don't have an email, Click here!
+        </Link>
       </form>
-      <Link to="/register" style={{textDecoration: 'none', color: '#9F765E'}}>
-        If you don't have an email, Click here!
-      </Link>
     </div>
   );
 };
--- src/components/Admin/Register.js
+++ src/components/Admin/Register.js
@@ -36,50 +36,108 @@ const Register = () => {
   };
 
   return (
-    <div style={{fontSize: '20px'}}>
-      <div>Login</div>
+    <div style={{padding: '20px'}}>
+      <div
+        style={{
+          fontSize: '22px',
+          color: 'rgb(77, 82, 86)',
+          marginBottom: '15px',
+        }}
+      >
+        Register
+      </div>
       <form onSubmit={onSubmit}>
         <div>
+          <div>
+            <label style={{fontSize: '18px', color: 'rgb(89, 89, 89)'}}>
+              Email
+            </label>
+          </div>
           <input
             type="email"
             value={email}
             placeholder="example@gmail.com"
-            style={{width: '300px', height: '20px'}}
+            style={{width: '300px', height: '25px'}}
             onChange={onChangeEmail}
           />
         </div>
         <div>
+          <div style={{marginTop: '20px'}}>
+            <label
+              style={{
+                fontSize: '18px',
+                color: 'rgb(89, 89, 89)',
+              }}
+            >
+              Name
+            </label>
+          </div>
           <input
             type="name"
             value={name}
             placeholder="Input your name"
-            style={{width: '300px', height: '20px', marginTop: '10px'}}
+            style={{width: '300px', height: '25px'}}
             onChange={onChangeName}
           />
+        </div>
+        <div style={{marginTop: '20px'}}>
+          <label
+            style={{
+              fontSize: '18px',
+              color: 'rgb(89, 89, 89)',
+            }}
+          >
+            Password
+          </label>
         </div>
         <div>
           <input
             type="password"
             value={password}
             placeholder="Input your password"
-            style={{width: '300px', height: '20px', marginTop: '10px'}}
+            style={{width: '300px', height: '25px'}}
             onChange={onChangePassword}
           />
+        </div>
+        <div style={{marginTop: '20px'}}>
+          <label
+            style={{
+              fontSize: '18px',
+              color: 'rgb(89, 89, 89)',
+            }}
+          >
+            Confirm Password
+          </label>
         </div>
         <div>
           <input
             type="password"
             value={password2}
             placeholder="Confirm Password"
-            style={{width: '300px', height: '20px', marginTop: '10px'}}
+            style={{width: '300px', height: '25px'}}
             onChange={onChangePassword2}
           />
         </div>
-        <button type="submit">Confirm</button>
+        <button
+          type="submit"
+          style={{
+            height: '2rem',
+            marginTop: '20px',
+            paddingLeft: '1.25rem',
+            paddingRight: '1.25rem',
+            borderRadius: '4px',
+            cursor: 'pointer',
+          }}
+        >
+          Confirm
+        </button>
+        <Link
+          to="/login"
+          style={{textDecoration: 'none', color: '#9F765E', marginLeft: '5px'}}
+        >
+          If you have an email, Click here!
+        </Link>
       </form>
-      <Link to="/login" style={{textDecoration: 'none', color: '#9F765E'}}>
-        If you have an email, Click here!
-      </Link>
     </div>
   );
 };
