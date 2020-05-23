4536568ccff8f713ecc7c3222bf9d81c4944c42a
Doowankim <rlaendhks11@gmail.com>
2020-05-23 17:42:00 +0900

    fix: Admit form style

--- src/components/Admin/Login.js
+++ src/components/Admin/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import useInputs from '../custom/useInputs';
 import FacebookLogin from 'react-facebook-login';
+import styled from 'styled-components';
 
 const Login = () => {
   const [state, onChange] = useInputs({
@@ -24,82 +25,118 @@ const Login = () => {
     console.log('responseFacebook');
   };
   return (
-    <div style={{padding: '20px'}}>
-      <div
-        style={{
-          fontSize: '22px',
-          color: 'rgb(77, 82, 86)',
-          marginBottom: '15px',
-        }}
-      >
-        Login
-      </div>
-      <form onSubmit={onSubmit}>
-        <div>
+    <Container>
+      <LoginBox>
+        <Header>Login</Header>
+        <form onSubmit={onSubmit}>
           <div>
-            <label style={{fontSize: '18px', color: 'rgb(89, 89, 89)'}}>
-              Email
-            </label>
+            <div>
+              <Label>Email</Label>
+            </div>
+            <Input
+              name="email"
+              type="email"
+              value={email}
+              placeholder="example@gmail.com"
+              onChange={onChange}
+            />
           </div>
-          <input
-            name="email"
-            type="email"
-            value={email}
-            placeholder="example@gmail.com"
-            style={{width: '300px', height: '25px'}}
-            onChange={onChange}
-          />
-        </div>
-        <div>
-          <div style={{marginTop: '20px'}}>
-            <label
-              style={{
-                fontSize: '18px',
-                color: 'rgb(89, 89, 89)',
-              }}
-            >
-              Password
-            </label>
+          <div>
+            <Wrapper>
+              <Label>Password</Label>
+            </Wrapper>
+            <Input
+              name="password"
+              type="password"
+              value={password}
+              placeholder="*******"
+              onChange={onChange}
+            />
           </div>
-          <input
-            name="password"
-            type="password"
-            value={password}
-            placeholder="*******"
-            style={{width: '300px', height: '25px'}}
-            onChange={onChange}
-          />
-        </div>
-        <button
-          type="submit"
-          style={{
-            height: '2rem',
-            marginTop: '20px',
-            paddingLeft: '1.25rem',
-            paddingRight: '1.25rem',
-            borderRadius: '4px',
-            cursor: 'pointer',
-          }}
-        >
-          Confirm
-        </button>
-        <Link
-          to="/register"
-          style={{textDecoration: 'none', color: '#9F765E', marginLeft: '5px'}}
-        >
-          If you don't have an email, Click here!
-        </Link>
-        <div style={{marginTop: '10px'}}>
-          <FacebookLogin
-            appId="386257415464246"
-            textButton="Facebook"
-            fields="name,email,picture"
-            callback={responseFacebook}
-          />
-        </div>
-      </form>
-    </div>
+          <Button type="submit">Confirm</Button>
+          <SLink to="/register">If you don't have an email, Click here!</SLink>
+          <Divider></Divider>
+          <SocialButton>
+            <FacebookLogin
+              appId="386257415464246"
+              textButton="Facebook"
+              fields="name,email,picture"
+              callback={responseFacebook}
+            />
+          </SocialButton>
+        </form>
+      </LoginBox>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  padding: 20px;
+`;
+
+const LoginBox = styled.div`
+  padding: 70px 0 30px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Header = styled.div`
+  font-size: 22px;
+  color: rgb(77, 82, 86);
+  margin-bottom: 15px;
+`;
+
+const Wrapper = styled.div`
+  margin-top: 20px;
+`;
+
+const Input = styled.input`
+  border: none;
+  height: 40px;
+  font-size: 18px;
+  color: #003764;
+  width: 100%;
+  vertical-align: middle;
+  border-bottom: 1px solid #cdd3e4;
+  border-radius: 0;
+  padding: 14px 0 0 0;
+`;
+
+const Label = styled.label`
+  font-size: 14px;
+  color: rgb(89, 89, 89);
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 56px;
+  min-height: 56px;
+  line-height: 1;
+  background-color: #0077c8;
+  margin: 20px auto 0 auto;
+  margin-bottom: 20px;
+  font-size: 14px;
+  color: white;
+  cursor: pointer;
+`;
+
+const SLink = styled(Link)`
+  text-decoration: none;
+  color: #9f765e;
+  padding-left: 60px;
+`;
+
+const Divider = styled.div`
+  width: 100%;
+  height: 1px;
+  margin-top: 20px;
+  padding-top: 20px;
+  border-top: 1px #bac0cd dotted;
+`;
+
+const SocialButton = styled.div`
+  margin-top: 10px;
+`;
 
 export default Login;
--- src/components/Admin/Register.js
+++ src/components/Admin/Register.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import styled from 'styled-components';
 import useInputs from '../custom/useInputs';
 
 const Register = () => {
@@ -24,114 +25,180 @@ const Register = () => {
   };
 
   return (
-    <div style={{padding: '20px'}}>
-      <div
-        style={{
-          fontSize: '22px',
-          color: 'rgb(77, 82, 86)',
-          marginBottom: '15px',
-        }}
-      >
-        Register
-      </div>
-      <form onSubmit={onSubmit}>
-        <div>
+    // <Container>
+    //   <RegisterBox>
+    //     <Header>Register</Header>
+    //     <form onSubmit={onSubmit}>
+    //       <div>
+    //         <div>
+    //           <Label>Email</Label>
+    //         </div>
+    //         <Input
+    //           name="email"
+    //           type="email"
+    //           value={email}
+    //           placeholder="example@gmail.com"
+    //           onChange={onChange}
+    //         />
+    //       </div>
+    //       <div>
+    //         <Wrapper>
+    //           <Label>Name</Label>
+    //         </Wrapper>
+    //         <Input
+    //           name="name"
+    //           type="name"
+    //           value={name}
+    //           placeholder="Input your name"
+    //           onChange={onChange}
+    //         />
+    //       </div>
+    //       <Wrapper>
+    //         <Label>Password</Label>
+    //       </Wrapper>
+    //       <div>
+    //         <Input
+    //           name="password"
+    //           type="password"
+    //           value={password}
+    //           placeholder="Input your password"
+    //           onChange={onChange}
+    //         />
+    //       </div>
+    //       <Wrapper>
+    //         <Label>Confirm Password</Label>
+    //       </Wrapper>
+    //       <div>
+    //         <Input
+    //           name="password2"
+    //           type="password"
+    //           value={password2}
+    //           placeholder="Confirm Password"
+    //           onChange={onChange}
+    //         />
+    //       </div>
+    //       <Button type="submit">Register</Button>
+    //       <Divider></Divider>
+    //       <SLink to="/login">If you have an email, Click here!</SLink>
+    //     </form>
+    //   </RegisterBox>
+    // </Container>
+
+    <Container>
+      <RegisterBox>
+        <Header>Register</Header>
+        <form onSubmit={onSubmit}>
           <div>
-            <label style={{fontSize: '18px', color: 'rgb(89, 89, 89)'}}>
-              Email
-            </label>
+            <div>
+              <Label>Email</Label>
+            </div>
+            <Input
+              name="email"
+              type="email"
+              value={email}
+              placeholder="example@gmail.com"
+              onChange={onChange}
+            />
           </div>
-          <input
-            name="email"
-            type="email"
-            value={email}
-            placeholder="example@gmail.com"
-            style={{width: '300px', height: '25px'}}
-            onChange={onChange}
-          />
-        </div>
-        <div>
-          <div style={{marginTop: '20px'}}>
-            <label
-              style={{
-                fontSize: '18px',
-                color: 'rgb(89, 89, 89)',
-              }}
-            >
-              Name
-            </label>
+          <div>
+            <Wrapper>
+              <Label>name</Label>
+            </Wrapper>
+            <Input
+              name="name"
+              type="name"
+              value={name}
+              placeholder="Input your name"
+              onChange={onChange}
+            />
           </div>
-          <input
-            name="name"
-            type="name"
-            value={name}
-            placeholder="Input your name"
-            style={{width: '300px', height: '25px'}}
-            onChange={onChange}
-          />
-        </div>
-        <div style={{marginTop: '20px'}}>
-          <label
-            style={{
-              fontSize: '18px',
-              color: 'rgb(89, 89, 89)',
-            }}
-          >
-            Password
-          </label>
-        </div>
-        <div>
-          <input
-            name="password"
-            type="password"
-            value={password}
-            placeholder="Input your password"
-            style={{width: '300px', height: '25px'}}
-            onChange={onChange}
-          />
-        </div>
-        <div style={{marginTop: '20px'}}>
-          <label
-            style={{
-              fontSize: '18px',
-              color: 'rgb(89, 89, 89)',
-            }}
-          >
-            Confirm Password
-          </label>
-        </div>
-        <div>
-          <input
-            name="password2"
-            type="password"
-            value={password2}
-            placeholder="Confirm Password"
-            style={{width: '300px', height: '25px'}}
-            onChange={onChange}
-          />
-        </div>
-        <button
-          type="submit"
-          style={{
-            height: '2rem',
-            marginTop: '20px',
-            paddingLeft: '1.25rem',
-            paddingRight: '1.25rem',
-            borderRadius: '4px',
-            cursor: 'pointer',
-          }}
-        >
-          Confirm
-        </button>
-        <Link
-          to="/login"
-          style={{textDecoration: 'none', color: '#9F765E', marginLeft: '5px'}}
-        >
-          If you have an email, Click here!
-        </Link>
-      </form>
-    </div>
+          <div>
+            <Wrapper>
+              <Label>Password</Label>
+            </Wrapper>
+            <Input
+              name="password"
+              type="password"
+              value={password}
+              placeholder="*******"
+              onChange={onChange}
+            />
+          </div>
+          <div>
+            <Wrapper>
+              <Label>Confirm Password</Label>
+            </Wrapper>
+            <Input
+              name="password2"
+              type="password"
+              value={password2}
+              placeholder="*******"
+              onChange={onChange}
+            />
+          </div>
+          <Button type="submit">Confirm</Button>
+          <SLink to="/login">If you have an email, Click here! </SLink>
+        </form>
+      </RegisterBox>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  padding: 20px;
+`;
+
+const RegisterBox = styled.div`
+  padding: 70px 0 30px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Header = styled.div`
+  font-size: 22px;
+  color: rgb(77, 82, 86);
+  margin-bottom: 15px;
+`;
+
+const Wrapper = styled.div`
+  margin-top: 20px;
+`;
+
+const Input = styled.input`
+  border: none;
+  height: 40px;
+  font-size: 18px;
+  color: #003764;
+  width: 100%;
+  vertical-align: middle;
+  border-bottom: 1px solid #cdd3e4;
+  border-radius: 0;
+  padding: 14px 0 0 0;
+`;
+
+const Label = styled.label`
+  font-size: 14px;
+  color: rgb(89, 89, 89);
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 56px;
+  min-height: 56px;
+  line-height: 1;
+  background-color: #0077c8;
+  margin: 20px auto 0 auto;
+  margin-bottom: 20px;
+  font-size: 14px;
+  color: white;
+  cursor: pointer;
+`;
+
+const SLink = styled(Link)`
+  text-decoration: none;
+  color: #9f765e;
+  padding-left: 80px;
+`;
 
 export default Register;
